1f88e0d95b18a42d83116727d48a1fda22a20953
Amine JAMAI <mr.maj.2015@gmail.com>
2021-02-21 16:27:03 +0100

    fixed Null check

--- src/main/java/com/titsuite/users/Customer.java
+++ src/main/java/com/titsuite/users/Customer.java
@@ -42,7 +42,7 @@ public class Customer extends User {
 
     public void setSubscription(String subscription) {
         for (String sub : subscriptions) {
-            if (subscription.equals(sub)) {
+            if (subscription!=null && subscription.equals(sub)) {
                 this.subscription = subscription;
                 return;
             }
